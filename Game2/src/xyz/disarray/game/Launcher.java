90a00f08ed366667fb357c0586b3e200de73a8c6
Kevin <electrichexagon8@gmail.com>
2020-04-18 14:54:10 -0700

    Moved message generator class into just a method

--- Game2/src/xyz/disarray/game/Launcher.java
+++ Game2/src/xyz/disarray/game/Launcher.java
@@ -1,11 +1,15 @@
 package xyz.disarray.game;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import processing.awt.PSurfaceAWT;
 import processing.core.PApplet;
-import xyz.disarray.game.util.MessageGenerator;
 
 public class Launcher {
 	public static void main(String[] args) {
@@ -32,7 +36,7 @@ public class Launcher {
 
 		window.setSize(800, 600);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setTitle("Epic - " + MessageGenerator.generate());
+		window.setTitle("Epic - " + generate());
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				window.setResizable(false);
@@ -41,5 +45,29 @@ public class Launcher {
 
 		window.setVisible(true);
 		canvas.requestFocus();
+	}
+	
+	/**
+	 * Selects a message at random from the messages text file to be displayed on the window's title.
+	 * 
+	 * @return Returns a random message from the messages text file.
+	 */
+	public static String generate() {
+		File file = new File("data/messages");
+		Scanner sc;
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			return "";
+		}
+
+		ArrayList<String> messages = new ArrayList<>();
+
+		while (sc.hasNextLine())
+			messages.add(sc.nextLine());
+
+		sc.close();
+
+		return messages.get((int) (Math.random() * messages.size()));
 	}
 }
