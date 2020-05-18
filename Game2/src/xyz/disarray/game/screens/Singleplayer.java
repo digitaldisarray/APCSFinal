2c2e5f7f56b6efc08f36055eeda7652083b21d49
Sam Leistiko <sleistiko297@student.fuhsd.org>
2020-05-17 19:31:43 -0700

    Zombie spawn rate increases exponentially, until a ceratin point. Made style epicer by removing some public static variables

--- Game2/src/xyz/disarray/game/screens/Singleplayer.java
+++ Game2/src/xyz/disarray/game/screens/Singleplayer.java
@@ -9,26 +9,30 @@ import xyz.disarray.game.entities.Wall;
 import xyz.disarray.game.entities.Zombie;
 
 public class Singleplayer extends Screen {
-	public static final double ZOMBIE_SPAWN_RATE = 0.003;
+	private double zombieSpawnRate = 0.003;
 	private ArrayList<Entity> entities;
 	private ArrayList<Wall> walls;
 	private LocalPlayer p;
+
 	public Singleplayer(LocalPlayer p) {
 		entities = new ArrayList<>();
 		this.p = p;
-		//entities.add(new Wall(80, 80, 100, 10));
-		
+		// entities.add(new Wall(80, 80, 100, 10));
 
 		// TODO: Get walls from map loader and add to entities
 
 	}
 
 	public void update() {
-		if(Math.random() < ZOMBIE_SPAWN_RATE)
-			entities.add(new Zombie((int)(Math.random()*800), (int)(Math.random()* 600)));
+		if (Math.random() < zombieSpawnRate) {
+			entities.add(new Zombie((int) (Math.random() * 800), (int) (Math.random() * 600)));
+			if(zombieSpawnRate < 0.015)
+			zombieSpawnRate *= 1.1;
+		}
 
 		for (Entity entity : entities)
 			entity.act();
+
 	}
 
 	public void draw(PApplet g) {
